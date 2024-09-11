c10eca65cab965cd5759c80a3273625a21bebfc0
Brian Lee <brian971002@gmail.com>
2024-09-11 07:40:21 -0400

    init: initialize graphQL apollo and live variables for authentication

---
+++ src/routers/logged-in-router.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { isLoggedInVar } from "../apollo";
+
+export const LoggedInRouter = () => {
+    return (
+        <div>
+            <h1>Logged In</h1>
+            <button onClick={()=>{isLoggedInVar(false)}}>Log out</button>
+        </div>
+    )
+}
