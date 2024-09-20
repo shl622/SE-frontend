df2ad5bcec8ba9c3e9f920971cd210d008002779
Brian Lee <brian971002@gmail.com>
2024-09-20 13:49:41 -0400

    feat: creates validation in login form

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,18 +1,36 @@
 import React from "react"
+import { useForm } from "react-hook-form"
+
+interface ILoginForm {
+    email?: string
+    password?: string
+}
 
 export const Login = () => {
+    const {register, getValues, formState: {errors}, handleSubmit } = useForm<ILoginForm>()
+    const onSubmit = () => {
+        console.log(getValues())
+    }
     return (
         <div className="h-screen flex items-center justify-center bg-gray-800 font-sans">
-            <div className="bg-white w-full max-w-screen-sm py-10 rounded-lg text-center">
+            <div className="bg-white w-full max-w-screen-sm pt-5 pb-7 rounded-lg text-center">
                 <h3 className="text-3xl text-gray-800"> Log in </h3>
-                <form className="flex flex-col mt-5 px-5">
+                <form onSubmit={handleSubmit(onSubmit)} className="grid gap-3 mt-5 px-5">
                     <input
-                        className="bg-gray-100 mb-3 py-3 px-5 rounded-lg shadow-inner focus:outline-none border-2 focus:border-green-800 focus:border-opacity-50 text-gray-800"
+                        {...register("email", {required: "Please enter a valid email"})}
+                        required
+                        className="input"
                         placeholder="Email" />
+                    {errors.email?.message && <span className="font-medium text-red-500">{errors.email.message}</span>}
                     <input
-                        className="bg-gray-100 py-3 px-5 rounded-lg shadow-inner focus:outline-none border-2 focus:border-green-800 focus:border-opacity-50 text-gray-800"
+                        {...register("password", {required: "Password is required", minLength: 5})}
+                        type="password"
+                        required
+                        className="input"
                         placeholder="Password" />
-                    <button className="bg-green-800 text-white py-2.5 rounded-lg shadow-inner focus:outline-none mt-3 text-lg hover:opacity-90">
+                    {errors.password?.message && <span className="font-medium text-red-500">{errors.password.message}</span>}
+                    {errors.password?.type === "minLength" && <span className="font-medium text-red-500">Password must be at least 5 characters</span>}
+                    <button className="mt-3 button">
                         Log in
                     </button>
                 </form>
