d8a7f45260acfd58dec85144c40e7a5c90060ca4
Brian Lee <brian971002@gmail.com>
2024-09-25 15:47:31 -0400

    feat: adds query for category list and restaurants list

--- src/pages/client/restaurants.tsx
+++ src/pages/client/restaurants.tsx
@@ -1,5 +1,57 @@
+import { gql, useQuery } from "@apollo/client";
 import React from "react";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { RestaurantsPageQueryQuery, RestaurantsPageQueryQueryVariables } from "../../__generated__/graphql";
+
+const RESTAURANTS_QUERY = gql`
+    query restaurantsPageQuery($input: RestaurantsInput!) {
+        allCategories{
+            ok
+            error
+            categories{
+                id
+                name
+                coverImg
+                slug
+                restaurantCount
+            }
+        }
+        restaurants(input: $input){
+            ok
+            error
+            totalPages
+            totalResults
+            results{
+                id
+                name
+                coverImg
+                category{
+                    name
+                }
+                address
+                isPromoted
+            }
+        }
+    }
+`
 
 export const Restaurants = () => {
-    return <h1>Client Restaurants</h1>
+    const {data, loading} = useQuery<RestaurantsPageQueryQuery, RestaurantsPageQueryQueryVariables>(RESTAURANTS_QUERY, {
+        variables: {
+            input: {
+                page: 1
+            }
+        }
+    })
+    console.log(data)
+    return(
+        <div>
+            <HelmetProvider>
+                <Helmet>
+                    <title>Restaurants | Super Eats</title>
+                </Helmet>
+            </HelmetProvider>
+            <h1>Restaurants</h1>
+        </div>
+    )
 }
--- src/pages/notfound.tsx
+++ src/pages/notfound.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 import { Link } from "react-router-dom";
 
 export const NotFound = () => {
     return (
         <div className="h-screen flex flex-col items-center justify-center">
+            <HelmetProvider>
+                <Helmet>
+                    <title>Not Found | Super Eats</title>
+                </Helmet>
+            </HelmetProvider>
             <h2 className="font-semibold text-2xl mb-3">Page Not Found.</h2>
             <h4 className="font-medium text-base mb-5">
                 The page you're looking for does not exist or has moved.
