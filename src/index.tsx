3d7a5019c6fee977e33abbc45d1bb3e552b6c061
Brian Lee <brian971002@gmail.com>
2024-09-25 22:38:46 -0400

    test: adds unit test for login logout routers on app.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './components/app';
 import reportWebVitals from './reportWebVitals';
 import "./styles/styles.css"
 import { ApolloProvider } from '@apollo/client';
