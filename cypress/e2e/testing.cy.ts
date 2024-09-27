045a76a8ea7c54b83a5107e89ed095a47df643ac
Brian Lee <brian971002@gmail.com>
2024-09-27 09:51:53 -0400

    feat: adds cypress testing library and tests for login bootup and error render

--- cypress/e2e/testing.cy.ts
+++ cypress/e2e/testing.cy.ts
@@ -1,5 +1,19 @@
-describe("first test", () => {
-    it("should go to the home page", () => {
-        cy.visit("http://localhost:3000").title().should("eq", "Login | Super Eats")
+describe("login page", () => {
+    it("should render the login page", () => {
+        cy.visit("/").title().should("eq", "Login | Super Eats")
+    })
+    it("should allow user to fill the form", () => {
+        cy.visit("/")
+        cy.findByPlaceholderText(/email/i).type("front1@gmail.com")
+        cy.findByPlaceholderText(/password/i).type("123456")
+        cy.findByRole("button")
+        cy.should("not.have.class", "pointer-events-none")
+        //to-do: user can login
+    })
+    it("should display error message for invalid email", () => {
+        cy.visit("/")
+        cy.findByPlaceholderText("Email").type("test")
+        cy.findByRole("alert")
+        cy.should("have.text", "Please enter a valid email")
     })
 })
