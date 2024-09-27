2a040b726bc9a03361144c176572b6411db584f3
Brian Lee <brian971002@gmail.com>
2024-09-27 10:03:50 -0400

    refactor: combine email and pw validation in one test and move to top

--- cypress/e2e/testing.cy.ts
+++ cypress/e2e/testing.cy.ts
@@ -3,6 +3,19 @@ describe("login page", () => {
     it("should render the login page", () => {
         user.visit("/").title().should("eq", "Login | Super Eats")
     })
+    it("should display validation errors", () => {
+        user.visit("/")
+        user.findByPlaceholderText(/email/i).type("test")
+        user.findByRole("alert")
+        user.should("have.text", "Please enter a valid email")
+        user.findByPlaceholderText(/email/i).clear().type("test@test.com")
+        user.findByPlaceholderText(/password/i).type("123")
+        user.findByRole("alert")
+        user.should("have.text", "Password must be at least 5 characters")
+        user.findByPlaceholderText(/password/i).clear()
+        user.findByRole("alert")
+        user.should("have.text", "Password is required")
+    })
     it("should allow user to fill the form", () => {
         user.visit("/")
         user.findByPlaceholderText(/email/i).type("front1@gmail.com")
@@ -10,22 +23,5 @@ describe("login page", () => {
         user.findByRole("button")
         user.should("not.have.class", "pointer-events-none")
         //to-do: user can login
-    })
-    it("should display error message for invalid email", () => {
-        user.visit("/")
-        user.findByPlaceholderText(/email/i).type("test")
-        user.findByRole("alert")
-        user.should("have.text", "Please enter a valid email")
-    })
-    it("should display error message for invalid password", () => {
-        user.visit("/")
-        user.findByPlaceholderText(/password/i).type("123")
-        user.findByRole("alert")
-        user.should("have.text", "Password must be at least 5 characters")
-        user.findByPlaceholderText(/password/i).clear()
-        user.findByPlaceholderText(/email/i).type("test@test.com")
-        user.findByPlaceholderText(/password/i).type("test").clear()
-        user.findByRole("alert")
-        user.should("have.text", "Password is required")
     })
 })
