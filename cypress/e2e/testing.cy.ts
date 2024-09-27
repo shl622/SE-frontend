8158d0f5e47c4e02ca1f6949fe45add5af1aa34b
Brian Lee <brian971002@gmail.com>
2024-09-27 09:52:40 -0400

    refactor: use cy as variable for readability

--- cypress/e2e/testing.cy.ts
+++ cypress/e2e/testing.cy.ts
@@ -1,19 +1,20 @@
 describe("login page", () => {
+    const user = cy
     it("should render the login page", () => {
-        cy.visit("/").title().should("eq", "Login | Super Eats")
+        user.visit("/").title().should("eq", "Login | Super Eats")
     })
     it("should allow user to fill the form", () => {
-        cy.visit("/")
-        cy.findByPlaceholderText(/email/i).type("front1@gmail.com")
-        cy.findByPlaceholderText(/password/i).type("123456")
-        cy.findByRole("button")
-        cy.should("not.have.class", "pointer-events-none")
+        user.visit("/")
+        user.findByPlaceholderText(/email/i).type("front1@gmail.com")
+        user.findByPlaceholderText(/password/i).type("123456")
+        user.findByRole("button")
+        user.should("not.have.class", "pointer-events-none")
         //to-do: user can login
     })
     it("should display error message for invalid email", () => {
-        cy.visit("/")
-        cy.findByPlaceholderText("Email").type("test")
-        cy.findByRole("alert")
-        cy.should("have.text", "Please enter a valid email")
+        user.visit("/")
+        user.findByPlaceholderText("Email").type("test")
+        user.findByRole("alert")
+        user.should("have.text", "Please enter a valid email")
     })
 })
